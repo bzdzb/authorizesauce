f0e32284088fdedb173239be1dfa40d71b393e83
bzdzb <brett@bzdzb.com>
2013-11-12 12:28:09 -0800

    Ensures run_tests are run against ../authorize

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -11,7 +11,7 @@ else:
 if __name__ == '__main__':
     tests_dir = os.path.dirname(os.path.abspath(__file__))
     authorize_dir = os.path.join(tests_dir, os.path.pardir)
-    sys.path.append(authorize_dir)
+    sys.path.insert(0, authorize_dir)
     suite = TestLoader().discover(tests_dir)
     runner = TextTestRunner(verbosity=1)
     runner.run(suite)
